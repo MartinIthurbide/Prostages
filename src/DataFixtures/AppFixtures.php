2b461efc31c46a71580d9b7f229bed60537002c9
Martin Ithurbide <mart.ithurbide@hotmail.com>
2019-12-18 13:30:33 +0100

    ajout de la premiere donnée de test de formation

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,17 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-
+use App\Entity\Entreprise;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $entrepriseUbisoft = new Entreprise();
+        $entrepriseUbisoft->setNom("Ubisoft");
+        $entrepriseUbisoft->setSiteWeb("https://www.ubisoft.com/fr-fr/");
+        $entrepriseUbisoft->setActivite("Concepteur de jeux vidéos");
+        $entrepriseUbisoft->setAdresse("114 Rue Lucien Faure, 33300 Bordeaux");
+        $manager->persist($entrepriseUbisoft);
 
         $manager->flush();
     }
