a00e5937a7fc22f6a4fc912e1f369d87d8bb28b1
Martin Ithurbide <mart.ithurbide@hotmail.com>
2019-12-19 15:55:04 +0100

    génératiob de données avec faker de formation et entreprise

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,17 +5,54 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Entreprise;
+use App\Entity\Formation;
+use App\Entity\Stage;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $entrepriseUbisoft = new Entreprise();
-        $entrepriseUbisoft->setNom("Ubisoft");
-        $entrepriseUbisoft->setSiteWeb("https://www.ubisoft.com/fr-fr/");
-        $entrepriseUbisoft->setActivite("Concepteur de jeux vidéos");
-        $entrepriseUbisoft->setAdresse("114 Rue Lucien Faure, 33300 Bordeaux");
-        $manager->persist($entrepriseUbisoft);
+        // Creation de générateur de données
+        $faker = \Faker\Factory::create('fr_FR');
+        $nombreOccurence = 10; // initialisation du nombre de peuplement à faire
 
-        $manager->flush();
+
+
+
+
+        // ENTREPRISES
+        for ($i=0; $i < $nombreOccurence ; $i++) { 
+            $entreprise = new Entreprise(); 
+            $nomEntreprise = $faker->company(); // Initialisation des noms d'entreprise aléatoire
+            $entreprise->setNom($nomEntreprise); // Définition des noms d'entreprises
+            $entreprise->setSiteWeb($faker->regexify('https://www\.'.$nomEntreprise.'\.fr')); // création d'un lien sur le site web avec le nom de l'entreprise
+            $entreprise->setActivite($faker->realText($maxNbChars = 100, $indexSize = 2)); // génération de texte aléatoire
+            $entreprise->setAdresse($faker->address()); // génération d'une adresse aléatoire
+            $manager->persist($entreprise);
+        }
+
+
+
+
+        // FORMATIONS
+
+         // Ajout des différents noms de formation
+        
+         $nomDesFormations = array(
+             // création des formations ainsi que leurs noms longs et courts
+            "DUT Info" => "DUT Information",
+            "DUT GIM" => "DUT Génie Industriel et Maintenance",
+            "DUT GEA" => "DUT Gestion des Entreprises et des Administrations"
+            );
+
+        foreach ($nomDesFormations as $nomCourt => $nomLong) {
+            $formation = new Formation(); 
+            $formation->setNomLong($nomLong); //association du nom long défini dans la création des formations
+            $formation->setNomCourt($nomCourt); //association du nom court défini dans la création des formations
+            $manager->persist($formation);
+        }
+
+       
+     $manager->flush();     
     }
+
 }
